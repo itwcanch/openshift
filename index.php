42a2ed975a65c4415ba2f7829f7f2f66617ae796
itwcanch <81386688+itwcanch@users.noreply.github.com>
2021-03-25 16:48:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo "< a href ="http://openshift-git-prueba-huawei.apps.openshift-01.gowecloud.com/test.php"> haga clic aquí para visitar la página de destino. < /a >"; ?>
+ <?php echo '<p>Hola Mundo</p>'; ?>
  </body>
 </html>
